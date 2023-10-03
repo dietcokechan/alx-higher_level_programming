2fd6e32abc6adb50d75d23ea8aeb1f9153346af7
dietcokechan <dietcokechan@gmail.com>
2023-10-03 12:36:22 +0300

    changed var name for pycodestyle

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,15 +2,14 @@
 import random
 number = random.randint(-10000, 10000)
 unsigned_digit = abs(number) % 10
-lastdigit = 0
+digit = 0
 if number < 0 and number != 0:
-    lastdigit = -unsigned_digit
+    digit = -unsigned_digit
 else:
-    lastdigit = unsigned_digit
-if lastdigit > 5:
-    print(f"Last digit of {number:d} is {lastdigit} and is greater than 5")
-elif lastdigit == 0:
-    print(f"Last digit of {number:d} is {lastdigit} and is 0")
-elif lastdigit < 6:
-    print(f"Last digit of {number:d} is {lastdigit} and is \
-    less than 6 and not 0")
+    digit = unsigned_digit
+if digit > 5:
+    print(f"Last digit of {number:d} is {digit} and is greater than 5")
+elif digit == 0:
+    print(f"Last digit of {number:d} is {digit} and is 0")
+elif digit < 6:
+    print(f"Last digit of {number:d} is {digit} and is less than 6 and not 0")

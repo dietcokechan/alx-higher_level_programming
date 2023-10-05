822b115f1221119a002dbd9557f62b4e9d3981b3
dietcokechan <dietcokechan@gmail.com>
2023-10-05 21:37:24 +0300

    added condition

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
-from sys import argv
+if __name__ == "__main__":
+    from sys import argv
 
-argcount = len(argv) - 1
+    argcount = len(argv) - 1
 
-if argcount == 1:
-    print("{} argument:".format(argcount))
-elif argcount == 0:
-    print("{} arguments.".format(argcount))
-else:
-    print("{} arguments:".format(argcount))
+    if argcount == 1:
+        print("{} argument:".format(argcount))
+    elif argcount == 0:
+        print("{} arguments.".format(argcount))
+    else:
+        print("{} arguments:".format(argcount))
 
-for a in range(1, argcount + 1):
-    print("{}: {}".format(a, argv[a]))
+    for a in range(1, argcount + 1):
+        print("{}: {}".format(a, argv[a]))

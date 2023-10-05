0e0139a4ea1913780de48d42aab3783f57bfd2a5
dietcokechan <dietcokechan@gmail.com>
2023-10-05 22:18:00 +0300

    added condition

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,5 +2,5 @@
 if __name__ == "__main__":
     import hidden_4
     for s in dir(hidden_4):
-        if s[:2] != "__":
+        if s[:2] != "__" and s[:1] != " \t":
             print("{}".format(s))

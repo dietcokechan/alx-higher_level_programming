ab7d52a8689dbaac79bbc7bbb0cc50fce35c4ee7
dietcokechan <dietcokechan@gmail.com>
2023-10-08 12:44:36 +0300

    defined data type

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,7 +2,7 @@
 def print_matrix_integer(matrix=[[]]):
     for i in matrix:
         for j in i:
-            print("{}".format(j), end="")
+            print("{:d}".format(j), end="")
             if j != i[len(i) - 1]:
                 print(" ", end="")
         print("$")

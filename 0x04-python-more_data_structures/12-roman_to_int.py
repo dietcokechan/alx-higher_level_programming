ecb33225fb3f6df27ad836e5d3aed3985b117ead
dietcokechan <dietcokechan@gmail.com>
2023-10-12 00:33:29 +0300

    pycodestyle

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,8 +2,8 @@
 def roman_to_int(roman_string):
     sum = 0
     if roman_string and isinstance(roman_string, str):
-        roman = {'D': 500, 'C': 100, 'M': 1000,
-                'L': 50, 'X': 10, 'V': 5, 'I': 1}
+        roman = \
+            {'D': 500, 'C': 100, 'M': 1000, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
         rlist = [roman.get(i) for i in roman_string]
         for i, char in enumerate(rlist):
             if i < len(rlist) - 1:

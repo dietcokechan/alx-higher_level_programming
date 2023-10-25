c5521ac93532d1e8eae04bb09fc480298aa4baae
dietcokechan <dietcokechan@gmail.com>
2023-10-25 14:26:17 +0300

    print visual square

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+"""define square class"""
+
+
+class Square:
+    """Square class
+    Attributes:
+        __size (int): size of square side
+    """
+    def __init__(self, size=0):
+        """Init a square
+        Args:
+            size(int): size of square side
+        Returns: None
+        """
+        self.__size = size
+
+    def area(self):
+        """Finds area of square
+        Returns: Area
+        """
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        """__size getter
+        Returns: size of square
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """__size setter
+        Args:
+            value (int): size of square
+        Returns: None
+        """
+        if type(value) is int:
+            if value < 0:
+                raise ValueError('size must be >= 0')
+            else:
+                self.__size = value
+        else:
+            raise TypeError('size must be an integer')
+
+    def my_print(self):
+        """Print square with #'s
+        Returns: visual square
+        """
+        if self.__size == 0:
+            print()
+            return
+        for i in range(self.__size):
+                print("".join(["#" for j in range(self.__size)]))

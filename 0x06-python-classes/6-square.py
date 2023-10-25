cffc122b0118075e9fe167cf1cab82e657d6fae5
dietcokechan <dietcokechan@gmail.com>
2023-10-25 16:22:08 +0300

    added condition

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -76,7 +76,8 @@ class Square:
             print()
             return
 
-        for i in range(self.__position[1]):
-            print()
+        if (self.__position[1] > 0):
+            for i in range(self.__position[1]):
+                print()
         for i in range(self.__size):
             print("{}{}".format(" " * self.__position[0], "#" * self.__size))

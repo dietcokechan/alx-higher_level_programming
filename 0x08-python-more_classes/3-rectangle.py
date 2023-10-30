6739d28a24ff00f0f593f065a22871f93f92cdf5
dietcokechan <dietcokechan@gmail.com>
2023-10-30 13:25:58 +0200

    print visual rectangle

---
+++ 0x08-python-more_classes/3-rectangle.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+"""rectangle class"""
+
+
+class Rectangle:
+    """initializes object"""
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """width getter"""
+        return self.__width
+
+    @property
+    def height(self):
+        """height getter"""
+        return self.__height
+
+    @width.setter
+    def width(self, value):
+        """width setter"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @height.setter
+    def height(self, value):
+        """height setter"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """returns area of rect"""
+        return self.width * self.height
+
+    def perimeter(self):
+        """returns perimeter of rect"""
+        if self.height == 0 or self.width == 0:
+            return 0
+        return (self.width + self.height) * 2
+
+    def __str__(self):
+        """changes str object"""
+        if not self.perimeter():
+            return ""
+        return("\n".join("#" * self.width for i in range(self.height)))

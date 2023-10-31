639b3ade7ecc1ce55c5a787ff65b4c3d91db2857
dietcokechan <dietcokechan@gmail.com>
2023-10-31 15:44:37 +0200

    pycdoestyle

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -5,11 +5,8 @@
 class LockedClass:
     """prevent user from dynamically creating new instance attr,
     except if the new instance attr is called first_name
-    
     Attributes:
         first_name (str): first name"""
-
-
     __slots__ = ["first_name"]
 
     def __init__(self):

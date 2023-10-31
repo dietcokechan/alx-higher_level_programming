afc46dac663d679e0ab5b7521680decefcddeb76
dietcokechan <dietcokechan@gmail.com>
2023-10-31 15:43:26 +0200

    fix?

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,8 +1,16 @@
 #!/usr/bin/python3
-
+"""class definition for locked class"""
 
 class LockedClass:
+    """prevent user from dynamically creating new instance attr,
+    except if the new instance attr is called first_name
+    
+    Attributes:
+        first_name (str): first name"""
+
+
     __slots__ = ["first_name"]
 
     def __init__(self):
+        """initialize class"""
         self.first_name = "first_name"

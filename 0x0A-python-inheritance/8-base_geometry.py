79d22c15e0dba249febc1736fc589e23eefcd79e
dietcokechan <dietcokechan@gmail.com>
2023-11-07 23:50:00 +0200

    rectangle class

---
+++ 0x0A-python-inheritance/8-base_geometry.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""base geometry"""
+
+
+class BaseGeometry:
+
+    def area(self):
+        """raise exception"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """validate value for integer and positive"""
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+
+"""class rectangle"""
+
+
+class Rectangle(BaseGeometry):
+    """define class"""
+
+    def __init__(self, width, height):
+        """initialize rectangle"""
+        if not super().integer_validator("width", width):
+            self.__width = width
+        if not super().integer_validator("height", height):
+            self.__height = height

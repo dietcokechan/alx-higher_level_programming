09d4730cece144ad1cb0cfb1f165c81b6f690d22
dietcokechan <dietcokechan@gmail.com>
2023-11-07 23:54:32 +0200

    square class

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+"""base geometry"""
+
+
+class BaseGeometry:
+    """define class"""
+    def area(self):
+        """raise exception"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """validate value for integer and positive"""
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+
+"""class rectangle"""
+
+
+class Rectangle(BaseGeometry):
+    """define class"""
+    def __init__(self, width, height):
+        """initialize rectangle"""
+        if not super().integer_validator("width", width):
+            self.__width = width
+        if not super().integer_validator("height", height):
+            self.__height = height
+
+    def area(self):
+        """return area"""
+        return self.__width * self.__height
+
+    def __str__(self):
+        """return string"""
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
+
+
+"""class square"""
+
+
+class Square(Rectangle):
+    """define class"""
+    def __init__(self, size):
+        """initialize rectangle"""
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def __str__(self):
+        """return string of square"""
+        return "[Square] {}/{}".format(self.__size, self.__size)

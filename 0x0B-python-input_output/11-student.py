787977cb494f58cbbcf9736b0c7d7dcea6ac8276
dietcokechan <dietcokechan@gmail.com>
2023-11-08 00:25:24 +0200

    define student class further

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""student class"""
+
+
+class Student:
+    """define class"""
+
+    def __init__(self, first_name, last_name, age):
+        """initialize class"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """prints __dict__"""
+        if (isinstance(attrs, list) and all(isinstance(i, str) for i in attrs)):
+            return ({i: j for i, j in self.__dict__.items() if i in attrs})
+        else:
+            return self.__dict__
+
+    def reload_from_json(self, json):
+        """replace attributes with json"""
+        if(json):
+            self.__dict__ = json

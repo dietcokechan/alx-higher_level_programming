964ae24863b718392e9ae62bce51557f236e0993
dietcokechan <dietcokechan@gmail.com>
2023-11-07 23:39:59 +0200

    raise exception

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""base geometry"""
+
+
+class BaseGeometry:
+    """define class"""
+    def __init__(self):
+        """initialize class"""
+        pass
+
+    def area(self):
+        """raise exception"""
+        raise Exception("area() is not implemented")

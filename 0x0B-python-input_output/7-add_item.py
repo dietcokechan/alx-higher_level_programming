75c0b10cf0fd499600cedea87816856344d31db8
dietcokechan <dietcokechan@gmail.com>
2023-11-08 00:16:56 +0200

    removed try/except

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -5,10 +5,7 @@ import sys
 save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 
-try:
-    jsonList = load_from_json_file("add_item.json")
-except Exception as ex:
-    jsonList = []
+jsonList = load_from_json_file("add_item.json")
 
 for i in sys.argv[1:]:
     jsonList.append(i)

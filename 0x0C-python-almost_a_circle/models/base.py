b06ca26476998225716c674ae916d7940258b65a
dietcokechan <dietcokechan@gmail.com>
2023-11-10 11:30:59 +0200

    rectangle class

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -7,6 +7,7 @@ class Base:
     __nb_objects = 0
 
     def __init__(self, id=None):
+        """initialization"""
         if id is not None:
             self.id = id
         else:
---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+"""rctangle class"""
+from models.base import Base
+
+
+class Rectangle(Base):
+    """class definition"""
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """initialization"""
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+        super().__init__(id)
+
+    @property
+    def width(self):
+        """getter"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """setter"""
+        self.__width = value
+
+    @property
+    def height(self):
+        """getter"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """setter"""
+        self.__height = value
+
+    @property
+    def x(self):
+        """getter"""
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """setter"""
+        self.__x = value
+
+    @property
+    def y(self):
+        """getter"""
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """setter"""
+        self.__y = value

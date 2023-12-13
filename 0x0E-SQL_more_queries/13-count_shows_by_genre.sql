13792c239ab8c62858bace1d73835ac7297c53f4
dietcokechan <dietcokechan@gmail.com>
2023-12-13 11:44:50 -0800

    lists all genres from db and displays num of linked shows

---
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,9 @@
+-- lists all genres from db and displays num of linked show
+SELECT tgs.name AS genre, COUNT(tg.genre_id) AS number_shows
+FROM tv_shows AS ts
+LEFT JOIN tv_show_genres AS tg
+ON ts.id = tg.show_id
+LEFT JOIN tv_genres AS tgs
+ON tg.genre_id = tgs.id
+WHERE tg.genre_id IS NOT NULL
+GROUP BY tgs.name ORDER BY number_shows DESC;

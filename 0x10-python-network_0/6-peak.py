4b920ea062f7a6803098105cdbb7765a0327d9b1
Habiba Sameh <68277372+dietcokechan@users.noreply.github.com>
2024-01-25 17:20:30 +0200

    doc

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -3,6 +3,7 @@
 
 
 def recur(low, high, nums):
+    """ recursively search """
     mid = (low + high) // 2
     if low == high:
         return nums[high]
@@ -10,7 +11,9 @@ def recur(low, high, nums):
         return(recur(mid + 1, high, nums))
     return(recur(low, mid, nums))
 
+
 def find_peak(list_of_integers):
+    """ find peak """
     if not list_of_integers:
         return
     return(recur(0, len(list_of_integers) - 1, list_of_integers))

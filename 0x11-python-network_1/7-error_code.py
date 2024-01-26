5142eed0188637fa87856252d19c981f72f6ec3a
Habiba Sameh <68277372+dietcokechan@users.noreply.github.com>
2024-01-27 01:44:32 +0200

    forgot a requirement

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -7,7 +7,7 @@ import requests
 if __name__ == "__main__":
     url = sys.argv[1]
     res = requests.get(url)
-    try:
+    if res.status_code >= 400:
+        print("Error code: {}".format(res.status_code))
+    else:
         print(res.text)
-    except requests.exceptions.RequestException as e:
-        print("Error code: {}".format(e.code))

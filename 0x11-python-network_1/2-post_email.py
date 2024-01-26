4e9d1b476124c5e35946c33f6c6d00e58cbd6a48
Habiba Sameh <68277372+dietcokechan@users.noreply.github.com>
2024-01-27 01:12:16 +0200

    wrong var

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -9,6 +9,6 @@ if __name__ == "__main__":
     url = sys.argv[1]
     val = {"email": sys.argv[2]}
     data = urllib.parse.urlencode(val).encode()
-    req = urllib.request.Request(url, val)
+    req = urllib.request.Request(url, data)
     with urllib.request.urlopen(req) as res:
         print(res.read().decode("utf-8"))

43cd7c1d28e902da458e8b3031e8346ab3fcee75
Habiba Sameh <68277372+dietcokechan@users.noreply.github.com>
2024-02-20 22:49:11 +0200

    spaces

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -3,9 +3,9 @@
 let fs = require('fs');
 let file = process.argv[2];
 fs.readFile(file, 'utf8', function (err, data = err) {
-    if (err) {
-        throw err;
-    } else {
-        console.log(data);
-    }
+  if (err) {
+    throw err;
+  } else {
+    console.log(data);
+  }
 });

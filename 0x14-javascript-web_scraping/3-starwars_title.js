bb61d58f053ab52f521857ece0dffa9efa762118
Habiba Sameh <68277372+dietcokechan@users.noreply.github.com>
2024-02-20 23:12:21 +0200

    format

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -3,10 +3,10 @@
 const request = require('request');
 const id = process.argv[2];
 
-request('http://swapi.co/api/films/' + id, (err, res, body) => {
-    if (err) {
-        console.log(err);
-    } else {
-        console.log(JSON.parse(body)['title']);
-    }
+request('https://swapi-api.alx-tools.com/api/films/' + id, (err, res, body) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(JSON.parse(body)['title']);
+  }
 });

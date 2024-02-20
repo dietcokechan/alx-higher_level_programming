b76a43a39f9f0bde19c98f6e48476da3325479f4
Habiba Sameh <68277372+dietcokechan@users.noreply.github.com>
2024-02-20 22:46:16 +0200

    syntax again

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -4,8 +4,9 @@ let fs = require('fs');
 let file = process.argv[2];
 
 fs.readFile(file, 'utf8', (err, data = err) => {
-    if (err)
+    if (err) {
         throw err;
-    else
+    } else {
         console.log(data);
+    }
 });

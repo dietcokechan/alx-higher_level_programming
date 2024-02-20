c478ba41427da44f15ded7c66a1cca0d2947a892
Habiba Sameh <68277372+dietcokechan@users.noreply.github.com>
2024-02-20 22:47:53 +0200

    switched function style

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,7 +2,7 @@
 
 let fs = require('fs');
 let file = process.argv[2];
-fs.readFile(file, 'utf8', (err, data = err) => {
+fs.readFile(file, 'utf8', function (err, data = err) {
     if (err) {
         throw err;
     } else {

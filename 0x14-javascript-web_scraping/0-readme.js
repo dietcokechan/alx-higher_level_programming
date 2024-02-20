947509afa5868b3d6dc45c19617d61bea337ebec
Habiba Sameh <68277372+dietcokechan@users.noreply.github.com>
2024-02-20 22:39:04 +0200

    readme

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+let fs = require('fs')
+let file = process.argv[2]
+
+fs.readFile(file, 'utf8', (err, data = err) => {
+    if (err)
+        throw err
+    else
+        console.log(data)
+})

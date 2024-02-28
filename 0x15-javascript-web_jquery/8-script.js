7029dea258bf8cdec3a9bfe7400fac8b1182da83
Habiba Sameh <68277372+dietcokechan@users.noreply.github.com>
2024-02-29 01:03:05 +0200

    star wars movies

---
+++ 0x15-javascript-web_jquery/8-script.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+    $.getJSON('https://swapi.co/https://swapi-api.alx-tools.com/api/films/?format=json/films/?format=json', function (data) {
+        $.each(data.results, function (index, value) {
+            $('UL#list_movies').append('<li>' + value.title + '</li>');
+        });
+    });
+});

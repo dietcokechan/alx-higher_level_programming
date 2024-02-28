ff3c7f62b2f6606847407b7976f1aefa7e54048c
Habiba Sameh <68277372+dietcokechan@users.noreply.github.com>
2024-02-29 01:05:54 +0200

    say hello

---
+++ 0x15-javascript-web_jquery/9-script.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    $.getJSON(
+        'https://hellosalut.stefanbohacek.dev/?lang=fr',
+        function (data) {
+            $.each(data.query.results, function (i, value) {
+                $('DIV#hello').text(value.hello);
+            });
+        }
+    );
+});
